75e7691b2bea7939f38851c28cd20f560ec2a189
ABHIJEET-YADAV <abhijeet1996yadav@gmail.com>
2018-10-23 22:52:20 +0530

    New Image

--- Translation.py
+++ Translation.py
@@ -7,7 +7,6 @@ height, width = img.shape[:2]
 print(height)
 print(width)
 
-
 quarter_height, quarter_width = height/4, width/4
 print(quarter_height)
 print(quarter_width)
@@ -18,7 +17,7 @@ T = np.float32([[1, 0, quarter_width],
 print(T)
 
 img_translation = cv2.warpAffine(img, T, (width, height))
-cv2.imshow('Original Image', img)
+cv2.imshow('OriginalImage', img)
 cv2.imshow('Tranlation', img_translation)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
