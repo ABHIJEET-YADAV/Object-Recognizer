f376f53cdc7f213e5d533fa4feba492fba0a573f
ABHIJEET-YADAV <abhijeet1996yadav@gmail.com>
2018-10-24 17:52:34 +0530

    Feature Files

--- Interplotation.py
+++ Interplotation.py
@@ -5,17 +5,17 @@ img = cv2.imread('Images/car.jpg')
 cv2.imshow('Original Image',img)
 cv2.waitKey(0)
 
-#size of our image 3/4 of it's original size
+#Size of image 3/4 of it's original size
 img_scaled = cv2.resize(img, None, fx=0.75, fy=0.75)
 cv2.imshow('Scaling -Linear Interplotation', img_scaled)
 cv2.waitKey()
 
-#doubled the size of our image
+#Doubled the size of image
 img_scaled1 = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
 cv2.imshow('Scaling - Cubic Interplotation', img_scaled1)
 cv2.waitKey()
 
-#skew the re-sizing by setting exact dimensions
+#Skew the re-sizing by setting exact dimensions
 img_scaled2 = cv2.resize(img, (900, 400), interpolation=cv2.INTER_AREA)
 cv2.imshow('Scaling - Skewed Size', img_scaled2)
 cv2.waitKey()
