72748d7d79ac60930b51ffa391d8bf3b95f8c0aa
ABHIJEET-YADAV <abhijeet1996yadav@gmail.com>
2018-11-04 21:37:42 +0530

    Image processing

--- Edge_Detection.py
+++ Edge_Detection.py
@@ -1,17 +1,12 @@
 import cv2
-import numpy as np
 
 def sketch(image):
-    #Convert image to grayscale
     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    #Cleanup image using gaussian blur
     img_gray_blur = cv2.GaussianBlur(img_gray,(5,5),0)
 
-    #Extract Edges
     canny_edges = cv2.Canny(img_gray_blur, 10, 70)
 
-    #Do an invert binarize the image
     ret, mask = cv2.threshold(canny_edges,70,255,cv2.THRESH_BINARY)
     return mask
 
