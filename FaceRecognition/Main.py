e287b39ffb2769434539ad1911cd1c8d2ed944f3
ABHIJEET-YADAV <abhijeet1996yadav@gmail.com>
2018-11-08 20:53:03 +0530

    Image processing

--- FaceRecognition/Main.py
+++ FaceRecognition/Main.py
@@ -8,3 +8,9 @@ data_path = 'FaceRecognition/Faces/'
 onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path,f))]
 
 Training_Data, Labels = [], []
+
+for i, files in enumerate(onlyfiles):
+    image_path = data_path + onlyfiles[i]
+    images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    Training_Data.append(np.asarray(images, dtype=np.uint8))
+    Labels.append(i)
