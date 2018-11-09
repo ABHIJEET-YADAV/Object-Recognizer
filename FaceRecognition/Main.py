d64f04f7b84a948ffc35764dda6ff51ee267f627
ABHIJEET-YADAV <abhijeet1996yadav@gmail.com>
2018-11-09 19:54:11 +0530

    Image procesing

--- FaceRecognition/Main.py
+++ FaceRecognition/Main.py
@@ -18,3 +18,14 @@ for i, files in enumerate(onlyfiles):
 print("Model Training Done")
 
 face_classifier = cv2.CascadeClassifier('"venv/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml"')
+
+def face_detector(img,size=0.5):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray,1.3,5)
+    if faces is():
+        return img,[]
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,255),2)
+        roi = img[y:y+h, x:x+w]
+        roi = cv2.resize(roi, (200,200))
+    return img,roi
