21c9acc8c9761af22f58c7b280f98381613e9a67
ABHIJEET-YADAV <abhijeet1996yadav@gmail.com>
2018-11-11 22:15:13 +0530

    Image processing

--- FaceRecognition/Main.py
+++ FaceRecognition/Main.py
@@ -4,7 +4,6 @@ from os import listdir
 from os.path import isfile, join
 
 data_path = 'FaceRecognition/Faces/'
-
 onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path,f))]
 
 Training_Data, Labels = [], []
@@ -15,19 +14,28 @@ for i, files in enumerate(onlyfiles):
     Training_Data.append(np.asarray(images, dtype=np.uint8))
     Labels.append(i)
 
-print("Model Training Done")
+Labels = np.asarray(Labels, dtype=np.int32)
 
-face_classifier = cv2.CascadeClassifier('"venv/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml"')
+model = cv2.face.LBPHFaceRecognizer_create()
 
-def face_detector(img,size=0.5):
+model.train(np.asarray(Training_Data), np.asarray(Labels))
+
+print("Model Training Complete!!!!!")
+
+face_classifier = cv2.CascadeClassifier('venv/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+
+def face_detector(img, size = 0.5):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray,1.3,5)
+
     if faces is():
         return img,[]
+
     for(x,y,w,h) in faces:
         cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,255),2)
         roi = img[y:y+h, x:x+w]
         roi = cv2.resize(roi, (200,200))
+
     return img,roi
 
 cap = cv2.VideoCapture(0)
